e2b0451d2b73408eb801cd92f96d6fab8e84c570
Aleksandr Urizchenko <urizcenkoaleksandr@gmail.com>
2021-04-29 17:18:26 +0300

    start work with Painter

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+       Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Instantiate(impactEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
